400032ef189d740e2da6236f1a6800f2a3d0f75c
ariel.plasencia <ariel.plasencia@gmail.com>
2022-05-02 16:24:10 -0400

    add tester of hitori

---
+++ src/Probador/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hitori;
+
+namespace Probador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Juego juego = new Juego(10, 1, 20);
+
+            juego.run();
+            
+        }
+        
+    }
+}
